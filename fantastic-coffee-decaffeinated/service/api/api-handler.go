dae0d91438812d98e3474f4fc42682f91077b58c
FrancescoPannozzo <pannozzo.699427@studenti.uniroma1.it>
2023-05-02 10:12:37 +0200

    remove example getHelloWorld

--- fantastic-coffee-decaffeinated/service/api/api-handler.go
+++ fantastic-coffee-decaffeinated/service/api/api-handler.go
@@ -7,7 +7,9 @@ import (
 // Handler returns an instance of httprouter.Router that handle APIs registered here
 func (rt *_router) Handler() http.Handler {
 	// Register routes
-	rt.router.GET("/", rt.getHelloWorld)
+
+	// example
+	//rt.router.GET("/", rt.getHelloWorld)
 	rt.router.GET("/context", rt.wrap(rt.getContextReply))
 
 	// loggin
